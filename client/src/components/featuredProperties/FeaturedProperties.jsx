ff2f1c9104af4b74af869081516526ef3be7b747
Rahul-vijaykumar-kulkarni <r40kulkarni@gmail.com>
2025-01-08 21:07:21 +0530

    Update FeaturedProperties.jsx

--- client/src/components/featuredProperties/FeaturedProperties.jsx
+++ client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -3,7 +3,7 @@ import "./featuredProperties.css";
 
 const FeaturedProperties = () => {
   // eslint-disable-next-line
-  const { data, loading,error } = useFetch("/hotels/?featured=true&limit=4");
+  const { data, loading, error } = useFetch("/hotels/?featured=true&limit=4");
 
   return (
     <div className="fp">
