68c7a66c5be42acf5231b7e092661f15a7f85aba
Rahul-vijaykumar-kulkarni <r40kulkarni@gmail.com>
2025-01-10 08:48:34 +0000

    Update FeaturedProperties.jsx  added empty array for data

--- client/src/components/featuredProperties/FeaturedProperties.jsx
+++ client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -1,36 +1,44 @@
 import useFetch from "../../hooks/useFetch";
-import "./featuredProperties.css";
+   import "./featuredProperties.css";
 
-const FeaturedProperties = () => {
-  // eslint-disable-next-line
-  const { data, loading, error } = useFetch("/hotels/?featured=true&limit=4");
+   const FeaturedProperties = () => {
+     const { data, loading, error } = useFetch("/hotels/?featured=true&limit=4");
+   
+       // Add a default value for data, assuming we might get an empty array if there are no featured properties.
+      const hotelData = data || [];
+   
+     return (
+       <div className="fp">
+         {loading ? (
+           "Loading"
+         ) : error ? (
+             "An error occurred while fetching data"
+          ) : (
+           <>
+             {hotelData.length > 0 ? (
+              hotelData.map((item) => (
+               <div className="fpItem" key={item._id}>
+                 <img src={item.photos[0]} alt="" className="fpImg" />
+                 <span className="fpName">{item.name}</span>
+                 <span className="fpCity">{item.city}</span>
+                 <span className="fpPrice">
+                   Starting from ${item.cheapestPrice}
+                 </span>
+                 {item.rating && (
+                   <div className="fpRating">
+                     <button>{item.rating}</button>
+                     <span>Excellent</span>
+                   </div>
+                 )}
+               </div>
+              ))
+             ) : (
+               <p>No featured properties found.</p> 
+             )}
+           </>
+         )}
+       </div>
+     );
+   };
 
-  return (
-    <div className="fp">
-      {loading ? (
-        "Loading"
-      ) : (
-        <>
-          {data.map((item) => (
-            <div className="fpItem" key={item._id}>
-              <img src={item.photos[0]} alt="" className="fpImg" />
-              <span className="fpName">{item.name}</span>
-              <span className="fpCity">{item.city}</span>
-              <span className="fpPrice">
-                Starting from ${item.cheapestPrice}
-              </span>
-              {item.rating && (
-                <div className="fpRating">
-                  <button>{item.rating}</button>
-                  <span>Excellent</span>
-                </div>
-              )}
-            </div>
-          ))}
-        </>
-      )}
-    </div>
-  );
-};
-
-export default FeaturedProperties;
+   export default FeaturedProperties;
